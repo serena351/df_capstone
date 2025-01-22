bdb0059ceb91e91fa4604b7a6b59539ae303e60d
serena351 <serenasenoo@hotmail.co.uk>
2025-01-22 09:39:46 +0000

    Starting to build Streamlit app

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import requests
 import altair as alt
 
-st.title("App")
+st.title("Medium Followers")
 
-user_input = st.text_input("Enter: ", "AAPL")
+# user_input = st.text_input("Enter a username: ", "AAPL")
 
-response = requests.get(f"https://randomapi.co/api/{user_input.lower()}")
-if response.status_code == 200:
-    image_url = response.json()['sprites']['front_default']
-    st.image(image_url, width=200) #, use_container_width=True
-else:
-    st.error('Response not found.')
+# response = requests.get(f"https://randomapi.co/api/{user_input.lower()}")
+# if response.status_code == 200:
+#     image_url = response.json()['sprites']['front_default']
+#     st.image(image_url, width=200) #, use_container_width=True
+# else:
+#     st.error('Response not found.')
