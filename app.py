3ee400a92c820db1b2d5e0db75d8b38bc71d1058
serena351 <serenasenoo@hotmail.co.uk>
2025-01-23 14:49:39 +0000

    Testing app minus API calls

--- app.py
+++ app.py
@@ -38,29 +38,29 @@ with tab1:
     st.subheader("Get User Information")
     st.write("Type in a Medium username (lowercase, no spaces) to get the user's information.")
     username = str(st.text_input("Enter a username: ", "timdenning"))
-    url = f"https://medium2.p.rapidapi.com/user/id_for/{username}"
-    headers = {
-	    "x-rapidapi-key": st.secrets["API_KEY"],
-	    "x-rapidapi-host": "medium2.p.rapidapi.com"
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        userid = response.json()['id']
-        url2 = f"https://medium2.p.rapidapi.com/user/{userid}"
-        response2 = requests.get(url2, headers=headers)
-        data = response2.json()
-        image_url = data.get('image_url')
-        user_info = {
-            'fullname': data.get('fullname'),
-            'followers_count': data.get('followers_count'),
-            'following_count': data.get('following_count'),
-            'top_writer_in': data.get('top_writer_in')
-        }
-        df = pd.DataFrame(user_info, index=[0])
-        st.image(image_url, width=100)
-        st.write(df)
-    else:
-	    st.error('Response not found.')
+    # url = f"https://medium2.p.rapidapi.com/user/id_for/{username}"
+    # headers = {
+	#     "x-rapidapi-key": st.secrets["API_KEY"],
+	#     "x-rapidapi-host": "medium2.p.rapidapi.com"
+    # }
+    # response = requests.get(url, headers=headers)
+    # if response.status_code == 200:
+    #     userid = response.json()['id']
+    #     url2 = f"https://medium2.p.rapidapi.com/user/{userid}"
+    #     response2 = requests.get(url2, headers=headers)
+    #     data = response2.json()
+    #     image_url = data.get('image_url')
+    #     user_info = {
+    #         'fullname': data.get('fullname'),
+    #         'followers_count': data.get('followers_count'),
+    #         'following_count': data.get('following_count'),
+    #         'top_writer_in': data.get('top_writer_in')
+    #     }
+    #     df = pd.DataFrame(user_info, index=[0])
+    #     st.image(image_url, width=100)
+    #     st.write(df)
+    # else:
+	#     st.error('Response not found.')
     
 with tab2:
     st.write("A chart of Tim Denning's followers over time:")
@@ -83,25 +83,25 @@ with tab2:
 with tab3:
     st.subheader("Get Article Information")
     st.caption("This section displays the top 10 articles of the user chosen in the first tab.")
-    url3 = f"https://medium2.p.rapidapi.com/user/{userid}/top_articles"
-    response3 = requests.get(url3, headers=headers)
+    # url3 = f"https://medium2.p.rapidapi.com/user/{userid}/top_articles"
+    # response3 = requests.get(url3, headers=headers)
     
-    if response3.status_code == 200:
-        articles = response3.json()['top_articles']
-        article_ids = [value for key, value in articles.items()]
+    # if response3.status_code == 200:
+    #     articles = response3.json()['top_articles']
+    #     article_ids = [value for key, value in articles.items()]
     
-        for article_id in article_ids:
-            url4 = f"https://medium2.p.rapidapi.com/article/{article_id}"  
-            response4 = requests.get(url4, headers=headers)
-            article_data = response4.json()
-            article_info = {
-            'title': article_data.get('title'),
-            'subtitle': article_data.get('subtitle'),
-            'claps': article_data.get('claps'),
-            }
-            st.write(article_info)
-    else: 
-        st.error('Response not found.')
+    #     for article_id in article_ids:
+    #         url4 = f"https://medium2.p.rapidapi.com/article/{article_id}"  
+    #         response4 = requests.get(url4, headers=headers)
+    #         article_data = response4.json()
+    #         article_info = {
+    #         'title': article_data.get('title'),
+    #         'subtitle': article_data.get('subtitle'),
+    #         'claps': article_data.get('claps'),
+    #         }
+    #         st.write(article_info)
+    # else: 
+    #     st.error('Response not found.')
       
     
 # Close the session
